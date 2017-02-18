231be115f437fc9b2689c5d12c5e63ddfcfffb15
John Gammie <John.Gammie@redflex.com.au>
2017-02-18 11:41:06 +1100

    adding webpack

---
+++ app/component.js
@@ -0,0 +1,10 @@
+/**
+ * Created by vajirad on 17/02/2017.
+ */
+module.exports = function(){
+    var element = document.createElement('h1');
+
+    element.innerHTML = 'Hello World' ;
+
+    return element;
+}
---
+++ app/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by vajirad on 17/02/2017.
+ */
+var component = require('./component');
+var app = document.createElement('div');
+
+document.body.appendChild(app);
+
+app.appendChild(component());
